11542124a8d702fab06eb395f6ce9c45a8fddbab
foxten <tenaysiafox@gmail.com>
2020-06-29 19:57:35 -0500

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,55 @@
 class Employee
+    attr_accessor :name, :manager, :department, :salary
+    # attr_reader :salary, :department, :manager
+    @@all = []
+    
+    # Employee#name
+    # returns a String that is the employee's name
+    # Employee#salary
+    # returns a Fixnum that is the employee's salary 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    # Employee#manager_name
+    # returns a String that is the name of their manager
+    def manager_name
+        self.manager.name
+        # Can be called using Employee.all[i], as opposed to a specific employee name since instances created by calling manager.hire_employee aren't given unique names.
+    end
 
+    # Employee.all
+    # returns an Array of all the employees
+    def self.all
+        @@all
+    end
+
+    # Employee.find_by_department
+    # takes a String argument that is the name of a department and returns the first employee whose manager is working in that department
+    def self.find_by_department(dept_name)
+        self.all.find do |employees| 
+            if employees.department == dept_name 
+                return employees.name
+            end
+        end
+    end
+
+
+    # Employee.paid_over
+    # takes a Fixnum argument and returns an Array of all the employees whose salaries are over that amount
+    def self.paid_over(amount)
+            self.all.select {|team| team.salary > amount}.map {|recipients| recipients.name} 
+    end
+
+
+    # Employee#tax_bracket
+    # returns an Array of all the employees whose salaries are within $1000 (± 1000) of the employee who invoked the method
+    def tax_bracket
+        lower_boundary = self.salary - 1000
+        upper_boundary = self.salary + 1000
+        Employee.all.select {|team| lower_boundary <= team.salary && team.salary <= upper_boundary}.map{|grouped| grouped.name}.reject{|names| names == self.name}
+        # Can be called using Employee.all[i], as opposed to a specific employee name since instances created by calling manager.hire_employee aren't given unique names.
+    end
 end
+#worked
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,52 @@
 class Manager
+    attr_accessor :name, :department, :age
+    @@all = []
+    # Manager#name
+    # returns a String that is the manager's name
+    # Manager#department
+    # returns a String that is the department that the manager oversees
+    # Manager#age
+    # returns a Fixnum that is the age of the manager
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+    
+    # Manager.all
+    # returns an Array of all the managers
+    def self.all
+        @@all 
+    end
 
+    # Manager#employees
+    # returns an Array of all the employees that the manager oversees
+    def employees
+        Employee.all.select {|employed| employed.manager == self}
+    end
+        
+    # Manager#hire_employee
+    # takes a String argument and a Fixnum argument of an employee's name and the employee's salary, respectively, and adds that employee to the list of employees that the manager oversees
+    def hire_employee(name, salary)
+        name = Employee.new(name)
+            name.salary = salary
+            name.manager = self
+            name.department = self.department
+
+    # Method works, but doesn't assign a unique name to each instance of the Employee class that it's created. 
+    end
+
+    # Manager.all_departments
+    # returns an Array of all the department names that every manager oversees
+    def self.all_departments
+        self.all.select {|mgmt| mgmt.department}.map {|dept| dept.department}
+    end
+    
+    # Manager.average_age
+    # returns a Float that is the average age of all the managers
+    def self.average_age
+        all_ages = self.all.map {|mgmt| mgmt.age}
+        all_ages.sum.to_f/all_ages.count
+    end
 end
